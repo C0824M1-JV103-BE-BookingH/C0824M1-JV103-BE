144871a33ae9278b8e2fe8c41b3272730a35249d
Le Tran Cong Dat <lephuonguyen021@gmail.com>
2025-05-06 20:32:20 +0700

    up gd booking

--- src/main/java/com/example/casestudy_g2_m4/controller/HomeController.java
+++ src/main/java/com/example/casestudy_g2_m4/controller/HomeController.java
@@ -2,12 +2,14 @@ package com.example.casestudy_g2_m4.controller;
 
 import com.example.casestudy_g2_m4.model.Hotel;
 import com.example.casestudy_g2_m4.service.hotel.IHotelService;
+import com.example.casestudy_g2_m4.service.roomtype.IRoomTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -16,6 +18,9 @@ import java.util.List;
 public class HomeController {
     @Autowired
     private IHotelService hotelService;
+
+    @Autowired
+    private IRoomTypeService roomTypeService;
 
     @GetMapping
     public String home(Model model, @RequestParam(value = "lang", required = false) String lang) {
@@ -37,5 +42,10 @@ public class HomeController {
     @GetMapping("/services")
     public String services(Model model, @RequestParam(value = "lang", required = false) String lang) {
         return "services_page";
+    }
+
+    @GetMapping("bookingNow")
+    public ModelAndView booking() {
+        return new ModelAndView("dashboard/book_now").addObject("booking", roomTypeService.findAllRoomType());
     }
 }
