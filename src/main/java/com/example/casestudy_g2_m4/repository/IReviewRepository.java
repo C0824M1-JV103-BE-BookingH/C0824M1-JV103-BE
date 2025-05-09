d7e9e690b8ad7a877fdb17c42e1438dd74b4198d
huyhoang2564 <huyhoang2564@.com>
2025-05-09 17:09:02 +0700

    add them file and fix

--- src/main/java/com/example/casestudy_g2_m4/repository/IReviewRepository.java
+++ src/main/java/com/example/casestudy_g2_m4/repository/IReviewRepository.java
@@ -2,6 +2,8 @@ package com.example.casestudy_g2_m4.repository;
 
 import com.example.casestudy_g2_m4.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface IReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByHotelId(Integer hotelId);
 }
--- src/main/java/com/example/casestudy_g2_m4/service/review/IReviewService.java
+++ src/main/java/com/example/casestudy_g2_m4/service/review/IReviewService.java
@@ -1,7 +1,9 @@
 package com.example.casestudy_g2_m4.service.review;
 
 import com.example.casestudy_g2_m4.model.Review;
+import java.util.List;
 
 public interface IReviewService {
     void save(Review review);
+    List<Review> findByHotelId(Integer hotelId);
 }
